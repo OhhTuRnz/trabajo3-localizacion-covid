ccd1a5475bdfeaa2b9cbc896e19c5a21764f5795
Ohhturnz <turnz7885@gmail.com>
2023-05-14 01:44:56 +0200

    Arreglando if-then-else anidados #4

--- src/main/java/com/practica/genericas/Coordenada.java
+++ src/main/java/com/practica/genericas/Coordenada.java
@@ -34,27 +34,17 @@ public class Coordenada {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
+		final int PRIME = 31;
 		int result = 1;
-		result = prime * result + Float.floatToIntBits(latitud);
-		result = prime * result + Float.floatToIntBits(longitud);
+		result = PRIME * result + Float.floatToIntBits(latitud);
+		result = PRIME * result + Float.floatToIntBits(longitud);
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
 		Coordenada other = (Coordenada) obj;
-		if (Float.floatToIntBits(latitud) != Float.floatToIntBits(other.latitud))
-			return false;
-		if (Float.floatToIntBits(longitud) != Float.floatToIntBits(other.longitud))
-			return false;
-		return true;
+		return (this == obj) || ((obj != null) && (getClass() == obj.getClass()) && (Float.floatToIntBits(latitud) == Float.floatToIntBits(other.latitud)) && (Float.floatToIntBits(longitud) == Float.floatToIntBits(other.longitud)));
 	}
 	
 }
